0b5afd587b896f3c092b02f49805656d08a64156
jaem <ksx4570@gmail.com>
2022-10-23 19:35:10 +0900

    react 연결

--- mydjango/mydjango/settings.py
+++ mydjango/mydjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     "post",
     "rest_framework",
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -51,6 +52,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
      'corsheaders.middleware.CorsMiddleware',    
     'django.middleware.common.CommonMiddleware', 
+
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -92,7 +94,7 @@ DATABASES = {
 }
 
 CORS_ORIGIN_WHITELIST = [
-    'localhost:3000/'
+    'http://localhost:3000',
 ]
 
 
